be103cb05572aff8984095843c7f0e4520385e40
Jared Fishman <jfish@gigbear.com>
2014-08-23 12:08:37 -0700

    Initial Commit

---
+++ DogBeacon/JFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JFAppDelegate.h
+//  DogBeacon
+//
+//  Created by Jared Fishman on 8/23/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DogBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DogBeacon
+//
+//  Created by Jared Fishman on 8/23/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JFAppDelegate class]));
+    }
+}
